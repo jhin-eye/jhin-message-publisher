2b2e3359a60a0326dcf909c46bcf509166c9ad8d
yyanoos <hyanoos0915@gmail.com>
2024-07-28 21:39:30 +0900

    chore: 커밋누락 커밋

---
+++ src/main/java/com/yanoos/message_publisher/MessagePublisherApplication.java
@@ -0,0 +1,13 @@
+package com.yanoos.message_publisher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessagePublisherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessagePublisherApplication.class, args);
+	}
+
+}
