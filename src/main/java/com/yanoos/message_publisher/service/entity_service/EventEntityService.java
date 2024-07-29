7c6d22b933733c088686098fad29847e08d43901
PC <yeonwoo_lee3@tmax.co.kr>
2024-07-29 20:41:02 +0900

    feat: EventEntityService 추가

---
+++ src/main/java/com/yanoos/message_publisher/service/entity_service/EventEntityService.java
@@ -0,0 +1,32 @@
+package com.yanoos.message_publisher.service.entity_service;
+
+import com.yanoos.message_publisher.entity.event.Event;
+import com.yanoos.message_publisher.entity.event.EventType;
+import com.yanoos.message_publisher.entity.event.MapEventEventType;
+import com.yanoos.message_publisher.repository.EventRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+@Transactional(readOnly = true)
+public class EventEntityService {
+    private final EventRepository eventRepository;
+
+    public Event getEventByEventId(long eventId){
+        Event event = eventRepository.findById(eventId).orElseThrow();
+        log.info("event {}", event.getEventData());
+        List<MapEventEventType> mapEventEventTypes = event.getMapEventEventTypes();
+        for(MapEventEventType mapEventEventType: mapEventEventTypes){
+            EventType eventType = mapEventEventType.getEventType();
+            log.info("ss: {}",mapEventEventType.getEventType().getEventType());
+        }
+        return event;
+    }
+}
