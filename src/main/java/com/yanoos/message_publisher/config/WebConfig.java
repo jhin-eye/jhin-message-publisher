8b59f6837182308b689b788ecd1dd31424adc1cf
PC <yeonwoo_lee3@tmax.co.kr>
2024-07-29 20:40:52 +0900

    fix: 필드주입 -> 생성자주입

--- src/main/java/com/yanoos/message_publisher/config/WebConfig.java
+++ src/main/java/com/yanoos/message_publisher/config/WebConfig.java
@@ -1,15 +1,15 @@
 package com.yanoos.message_publisher.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
+@RequiredArgsConstructor
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    @Autowired
-    private LoggingInterceptor loggingInterceptor;
+    private final LoggingInterceptor loggingInterceptor;
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
